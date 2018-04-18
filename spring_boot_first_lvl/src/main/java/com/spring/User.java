91dcfc000ab72d74691923256a6745c1d0ed165a
stepooleg <stepooleg@gmail.com>
2018-04-18 14:16:48 +0300

    spring boor 01

---
+++ spring_boot_first_lvl/src/main/java/com/spring/User.java
@@ -0,0 +1,49 @@
+package com.spring;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
