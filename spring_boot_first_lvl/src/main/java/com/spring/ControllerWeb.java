c5a97e851edb1b56c492168b5286a17f3d8ad1eb
stepooleg <stepooleg@gmail.com>
2018-04-18 14:45:45 +0300

    spring boot +freemarker

--- spring_boot_first_lvl/src/main/java/com/spring/ControllerWeb.java
+++ spring_boot_first_lvl/src/main/java/com/spring/ControllerWeb.java
@@ -2,15 +2,13 @@ package com.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class ControllerWeb {
 
     @RequestMapping("/")
-    @ResponseBody
-    public User setUser(){
-
-        return new User(1,"Ivan",23);
+    public String setUser(){
+        return "index";
     }
 }
